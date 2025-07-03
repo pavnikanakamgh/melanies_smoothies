17b844ae9e9560704dad97db9d33b097d4ee297f
pavnikanakamgh <pavnikanakam@gmail.com>
2025-07-03 17:07:59 -0400

    Update streamlit_app.py
    Corrected snowflake spelling

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on the Smoothie will be", name_on_order)
 
 
-cnx = st.connection("snowfalke")
+cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
